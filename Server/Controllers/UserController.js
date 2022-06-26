9f94cc9e4ffd88f5ae0ad2fddea4ecbfdfacf910
lamdarv <lamda.richo.tif420@polban.ac.id>
2022-06-26 21:58:13 +0700

    update backend

---
+++ Server/Controllers/UserController.js
@@ -0,0 +1,112 @@
+import UserModel from "../Models/UserModel.js";
+import bcrypt from 'bcrypt'
+
+// get a User
+export const getUser = async(req, res)=> {
+    const id = req.params.id;
+    try {
+        const user = await UserModel.findById(id);
+        if(user){
+            const{password, ...otherDetails} = user._doc
+            res.status(200).json(otherDetails)
+        } else {
+            res.status(404).json("No such user exists!")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+//update a user
+export const updateUser = async(req, res) => {
+    const id = req.params.id
+    const {
+        currentUserId,
+        currentUserAdminStatus,
+        password
+    } = req.body
+
+    if (id===currentUserId || currentUserAdminStatus){
+        try {
+            if(password){
+                const salt = await bcrypt.genSalt(10)
+                req.body.password = await bcrypt.hash(password, salt)
+            }
+            const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true})
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("Access Denied! You can only update your own profile")
+    }
+}
+
+//delete User
+export const deleteUser = async(req, res) => {
+    const id = req.params.id
+    const {
+        currentUserId,
+        currentUserAdminStatus
+    } = req.body
+    if (currentUserId === id || currentUserAdminStatus){
+        try {
+            await UserModel.findByIdAndDelete(id)
+            res.status(200).json("User deleted succesfully")
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("Access Denied! You can only delete your own profile")
+    }
+}
+
+//Follow a user
+export const followUser = async(req, res) => {
+    const id = req.params.id
+    const {currentUserId} = req.body
+
+    if(currentUserId === id){
+        res.status(403).json("Action forbidden")
+    } else {
+        try {
+            const followUser = await UserModel.findById(id)
+            const followingUser = await UserModel.findById(currentUserId)
+ 
+            if(!followUser.followers.includes(currentUserId)){
+                await followUser.updateOne({$push : {followers: currentUserId}})
+                await followingUser.updateOne({$push : {following: id}})
+                res.status(200).json("User followed!")
+            } else {
+                res.status(403).json("User is Already followed by you")
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+}
+
+//Unfollow a user
+export const unFollowUser = async(req, res) => {
+    const id = req.params.id
+    const {currentUserId} = req.body
+
+    if(currentUserId === id){
+        res.status(403).json("Action forbidden")
+    } else {
+        try {
+            const followUser = await UserModel.findById(id)
+            const followingUser = await UserModel.findById(currentUserId)
+ 
+            if(followUser.followers.includes(currentUserId)){
+                await followUser.updateOne({$pull : {followers: currentUserId}})
+                await followingUser.updateOne({$pull : {following: id}})
+                res.status(200).json("User unfollowed!")
+            } else {
+                res.status(403).json("User is not followed by you")
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+}
---
+++ Server/Routes/UserRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { deleteUser, followUser, getUser, unFollowUser, updateUser } from "../Controllers/UserController.js";
+
+const router = express.Router();
+
+// router.get('/', async(req,res)=>{
+//     res.send("user route")
+// })
+router.get('/:id', getUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', unFollowUser)
+
+export default router;
